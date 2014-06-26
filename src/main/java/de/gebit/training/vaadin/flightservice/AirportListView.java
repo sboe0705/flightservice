ff930ca59d58ca88afe6b4f8d1d28be1690ce621
sboehm <sebastian.boehm@gebit.de>
2014-06-26 10:37:04 +0200

    Airport-List implementiert

--- src/main/java/de/gebit/training/vaadin/flightservice/AirportListView.java
+++ src/main/java/de/gebit/training/vaadin/flightservice/AirportListView.java
@@ -1,20 +1,33 @@
 package de.gebit.training.vaadin.flightservice;
 
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
+
+import de.gebit.training.vaadin.workshop.service.Airport;
+import de.gebit.training.vaadin.workshop.service.TravelService;
+import de.gebit.training.vaadin.workshop.service.TravelServiceFactory;
 
 public class AirportListView extends CustomComponent implements View {
 
 	private static final long serialVersionUID = 1L;
 
+	private final BeanItemContainer<Airport> container = new BeanItemContainer<>(Airport.class);
+	
 	public AirportListView() {
-		setCompositionRoot(new Label("AirportList"));
+		Table table = new Table();
+		table.setContainerDataSource(container);
+		table.setVisibleColumns("id", "name", "city");
+		table.setSizeFull();
+		setCompositionRoot(table);
 	}
 
 	public void enter(ViewChangeEvent event) {
-
+		TravelService travelService = TravelServiceFactory.getInstance();
+		container.removeAllItems();
+		container.addAll(travelService.getAirports());
 	}
 
 }
